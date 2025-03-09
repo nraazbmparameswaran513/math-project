18244aa18e4bcefe581da8e70536898f1f741fce
nraazbmparameswaran513 <nraazbmparameswaran513@gmail.com>
2025-03-09 18:46:43 +0000

    Add 4.py

---
+++ src/4.py
@@ -0,0 +1,16 @@
+# Math Project Code
+# Returns the sum of two numbers
+def add(num1, num2):
+  return num1 + num2
+
+# Returns the difference of two numbers
+def subtract(num1, num2):
+  return num1 - num2
+
+# Returns the product of two numbers
+def multiply(num1, num2):
+  return num1 * num2
+
+# Returns the quotient of two numbers
+def divide(num1, num2):
+  return num1 / num2

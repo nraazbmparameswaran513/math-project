84f69584a26bbd5125cec492c65da0678690e99f
nraazbmparameswaran513 <nraazbmparameswaran513@gmail.com>
2025-03-16 17:45:23 +0000

    Add 11.py

---
+++ src/11.py
@@ -0,0 +1,10 @@
+import random
+def get_random_python_code():
+return f"""
+# Random Python Code
+
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
+result = num1 + num2
+print(f'The sum of {num1} and {num2} is {result}.')
+"""

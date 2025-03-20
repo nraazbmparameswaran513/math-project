31e553b24bdb687e5fb59f25ef3e5940d7ba866d
nraazbmparameswaran513 <nraazbmparameswaran513@gmail.com>
2025-03-20 17:50:53 +0000

    Add 15.py

---
+++ src/15.py
@@ -0,0 +1,9 @@
+import math
+a = 5
+b = 10
+c = 15
+
+print(f"Sum of {a} and {b} is: {a + b}")
+print(f"Difference between {a} and {b} is: {abs(a - b)}")
+print(f"Product of {a} and {b} is: {a * b}")
+print(f"Quotient of {a} and {b} is: {math.floor(a / b)}")
